168938323a196f4e2d5f9a34244e927d0eff6e6e
Vladimir_Sereda <vovasereda541@gmail.com>
2024-10-19 16:55:37 +0300

    header and main

--- main/main.cpp
+++ main/main.cpp
@@ -2,8 +2,16 @@
 #include <algorithm>
 #include <string>
 #include <iostream>
-
+#include <list>
+#include <chrono>
+#include <fstream>
+#include <data_generator.h>
 
 using namespace std;
 
+#define RUN(x) {                \
+    Run(#x, x, data);           \
+}
+
+template<typename T>
 
---
+++ main/sort_algorithms.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <list>
+
+void heapSort(std::list<int>& data);
+void treeSort(std::list<int>& data);
+void mergeSort(std::list<int>& data);
+void gnomeSort(std::list<int>& data);
+void shakerSort(std::list<int>& data);
