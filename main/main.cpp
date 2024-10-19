4ca63e6e4383cceef7bab08dd2c689a748864b22
Vladimir_Sereda <vovasereda541@gmail.com>
2024-10-19 17:41:25 +0300

    cout

--- main/main.cpp
+++ main/main.cpp
@@ -13,5 +13,17 @@ using namespace std;
     Run(#x, x, data);           \
 }
 
-template<typename T>
+template<typename T, typename SortMethod>
+void Run(const string& method_name, SortMethod sort_method, const list<T>& data) {
+    cout << "Running the algorithm: " << method_name << endl;
 
+    auto start = chrono::high_resolution_clock::now();
+    list<T> sorted_data = data;
+    auto stop = chrono::system_clock::now();
+    auto time = chrono::duration_cast<chrono::microseconds>(stop - start).count();
+
+    bool is_sorted = std::is_sorted(sorted_data.begin(), sorted_data.end());
+
+    cout << "Algorithm " << method_name << (is_sorted ? " completed successfully." : " completed with an error.") << endl;
+cout << "Time: " << duration << " mcs" << endl << endl;
+}
