24f364ca187c5c8f6d9d4b2f8c5227bad99d293b
kyozxtsu <rizuku1337@gmail.com>
2024-10-22 16:58:56 +0300

    Create ShakerSortYakimchik.cpp
    Реализация алгоритма шейкерной сортировки(коктейльной)

---
+++ main/ShakerSortYakimchik.cpp
@@ -0,0 +1,40 @@
+#include <sort_algorithms.h>
+#include <iostream>
+#include <list>
+
+void ShakerSort(std::list<int>& data) {
+    void cocktailSort(std::list<int>& lst) {
+    bool swapped = true;
+    auto start = lst.begin();
+    auto end = lst.end();
+    --end;
+
+    while (swapped) {
+        swapped = false;
+
+        for (auto it = start; it != end; ++it) {
+            auto next = std::next(it);
+            if (*it > *next) {
+                std::swap(*it, *next);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+
+        swapped = false;
+        --end;
+
+        for (auto it = end; it != start; --it) {
+            auto prev = std::prev(it);
+            if (*prev > *it) {
+                std::swap(*prev, *it);
+                swapped = true;
+            }
+        }
+
+        ++start;
+    }
+    }
+}
