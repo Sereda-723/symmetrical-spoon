1974e56749b559d0429b11c6f5fd0296dfc6e4c1
KateFlash <katkatcloudnikitina@gmail.com>
2024-10-27 22:11:49 +0300

    2
    Изменила lst на data, поменяла название файла

---
+++ main/gnomeSort_Nikitina.cpp
@@ -0,0 +1,32 @@
+#include <sort_algorithms.h>
+#include <iostream>
+#include <list>
+
+void gnomeSortNikitina(std::list<int>& data) {
+    if (data.size() < 2) {
+        return;
+    }
+
+    auto it = data.begin();
+    auto prev = it;
+
+    std::advance(it, 1);
+
+    while (it != data.end()) {
+        if (*prev <= *it) {
+            prev = it;
+            ++it;
+        } else {
+            std::iter_swap(it, prev);
+
+            if (prev != data.begin()) {
+                --it;
+                --prev;
+            } else {
+                ++it;
+                prev = it;
+                --prev;
+            }
+        }
+    }
+}
