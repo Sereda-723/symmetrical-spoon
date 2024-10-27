eaa55af7dccd83f86dcfd5bd90edc22e4241dcf2
bbatman1 <egor.batmanov123@gmail.com>
2024-10-27 14:45:41 +0300

    изменение на list

--- main/bbatman.cpp
+++ main/bbatman.cpp
@@ -1,84 +1,54 @@
-// #include <sort_algorithms.h>
+#include <sort_algorithms.h> 
+#include <iostream> 
+#include <list> 
 
+void insertionSortSereda(std::list<int>& data) {
 
-// void insertionSortSereda(std::list<int>& data) {
+}
 
-// }
+using namespace std;
 
-// #include <iostream>
-// #include <list>
+void merge(list<int>& arr, list<int>::iterator left, list<int>::iterator mid, list<int>::iterator right) {
+    list<int> L(left, mid);
+    list<int> R(mid, right);
 
-// using namespace std;
+    auto it = left;
+    auto itL = L.begin();
+    auto itR = R.begin();
 
-// // ������� ��� ������� ���� ��������������� ����������
-// void merge(list<int>& arr, list<int>::iterator left, list<int>::iterator mid, list<int>::iterator right) {
-//     // ������� ��������� ������
-//     list<int> L(left, mid);
-//     list<int> R(mid, right);
+    while (itL != L.end() && itR != R.end()) {
+        if (*itL <= *itR) {
+            *it = *itL;
+            itL++;
+        }
+        else {
+            *it = *itR;
+            itR++;
+        }
+        it++;
+    }
 
-//     // ������� ��������������� ���������� � �������� ������
-//     auto it = left; // �������� ��� ��������� ������
-//     auto itL = L.begin();
-//     auto itR = R.begin();
+    while (itL != L.end()) {
+        *it = *itL;
+        itL++;
+        it++;
+    }
+    while (itR != R.end()) {
+        *it = *itR;
+        itR++;
+        it++;
+    }
+}
 
-//     while (itL != L.end() && itR != R.end()) {
-//         if (*itL <= *itR) {
-//             *it = *itL;
-//             itL++;
-//         }
-//         else {
-//             *it = *itR;
-//             itR++;
-//         }
-//         it++;
-//     }
+void mergeSort(list<int>& arr) {
+    left = arr.begin();
+    right = arr.end();
+    if (distance(left, right) > 1) {
+        auto mid = next(left, distance(left, right) / 2);
 
-//     // �������� ���������� �������� (���� ����)
-//     while (itL != L.end()) {
-//         *it = *itL;
-//         itL++;
-//         it++;
-//     }
-//     while (itR != R.end()) {
-//         *it = *itR;
-//         itR++;
-//         it++;
-//     }
-// }
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid, right);
 
-// // ����������� ������� ��� ���������� ��������
-// void mergeSort(list<int>& arr, list<int>::iterator left, list<int>::iterator right) {
-//     if (distance(left, right) > 1) { // ���������, �� �������� �� ������ ������ ��� �� �������� �� �� ������ ���� �������
-//         // ������� �������� ������
-//         auto mid = next(left, distance(left, right) / 2);
-
-//         // ���������� ��������� ����� � ������ �����
-//         mergeSort(arr, left, mid);
-//         mergeSort(arr, mid, right);
-
-//         // ������� ��������������� �����
-//         merge(arr, left, mid, right);
-//     }
-// }
-
-// int main() {
-//     list<int> arr = { 12, 11, 13, 5, 6, 7 };
-
-//     setlocale(LC_ALL, "RU");
-
-//     cout << "�������� ������: ";
-//     for (auto it = arr.begin(); it != arr.end(); it++) {
-//         cout << *it << " ";
-//     }
-//     cout << endl;
-
-//     mergeSort(arr, arr.begin(), arr.end());
-
-//     cout << "��������������� ������: ";
-//     for (auto it = arr.begin(); it != arr.end(); it++) {
-//         cout << *it << " ";
-//     }
-//     cout << endl;
-
-//     return 0;
-// }
+        merge(arr, left, mid, right);
+    }
+}
