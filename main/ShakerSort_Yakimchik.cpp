b7bf7260a9ca6ba133c3bb7730d7155e62616bab
kyozxtsu <rizuku1337@gmail.com>
2024-10-27 22:15:33 +0300

    3
    Изменил название .cpp файла;
    Изменил название функции сортировки, чтобы не было конфликтов с main.cpp,;
    Заменил lst на data

---
+++ main/ShakerSort_Yakimchik.cpp
@@ -0,0 +1,38 @@
+#include <sort_algorithms.h>
+#include <iostream>
+#include <list>
+
+void shakerSort(std::list<int>& data) {
+    bool swapped = true;
+    auto start = data.begin();
+    auto end = data.end();
+    --end;
+
+    while (swapped) {
+        swapped = false;
+
+        for (auto it = start; it != end; ++it) {
+            auto next = std::next(it);
+            if (*it > *next) {
+                std::swap(*it, *next);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+            break;
+
+        swapped = false;
+        --end;
+
+        for (auto it = end; it != start; --it) {
+            auto prev = std::prev(it);
+            if (*prev > *it) {
+                std::swap(*prev, *it);
+                swapped = true;
+            }
+        }
+
+        ++start;
+    }
+}
